d0de6fe29835e876a7d97b75eee4aaba2f3f72b3
Omdeep <omdeep1990@gmail.com>
2022-09-09 10:24:23 +0530

    numberPicker, datePicker, TimePicker Dialog,webView

---
+++ app/src/main/java/com/chetu/demotp8/NumberActivity.java
@@ -0,0 +1,47 @@
+package com.chetu.demotp8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.NumberPicker;
+
+import com.chetu.demotp8.databinding.ActivityNumberBinding;
+
+public class NumberActivity extends AppCompatActivity {
+    private ActivityNumberBinding binding;
+    //TODO: 2.
+    private String[] monthName = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityNumberBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //TODO: 1. For Picking number only;
+        binding.numberPicker.setMinValue(0);
+        binding.numberPicker.setMaxValue(100);
+
+        binding.numberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int oldValue, int newValue) {
+                binding.showNumber.setText("Current Value : "+String.valueOf(numberPicker.getValue())+"\n"+
+                        "Old Value :"+String.valueOf(oldValue)+"\n"+
+                        "New Value :"+String.valueOf(newValue));
+            }
+        });
+
+
+        //TODO: 2. Month Name Picker from the given Array: -
+        binding.monthPicker.setMinValue(0);
+        binding.monthPicker.setMaxValue(11);
+        binding.monthPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int oldValue, int newValue) {
+                binding.showMonth.setText("Current Value : "+monthName[numberPicker.getValue()]+"\n"+
+                        "Old Value :"+monthName[oldValue]+"\n"+
+                        "New Value :"+monthName[newValue]);
+            }
+        });
+    }
+}
